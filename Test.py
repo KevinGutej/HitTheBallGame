033869b0350d9db0ef2ba2b363580795a7741696
Kevin <kevisrich144@gmail.com>
2021-10-27 15:07:15 +0100

    The ball hits all side and the side it bounces of shows a message left right up down.

--- Test.py
+++ Test.py
@@ -9,27 +9,41 @@ canvas = Canvas(tk, width=500, height=400, bd=0,highlightthickness=0)#No border 
 canvas.pack()
 tk.update()
 
-class Ball:#Class created called ball
-    def __init__(self,canvas,color):#function init "__" = magic function (change behavor of function).
+class Ball:#Class created called ball.
+    def __init__(self,canvas,kolor):#function init "__" = magic function (change behavor of function).
         self.canvas = canvas
-        self.id =  canvas.create_rectangle(100,100,100,100, fill=color)#reading function canvas.create.oval and their parameters.
-        self.canvas.move(self.id, 30,300)#We are moving the oval to the center of the canvas.
-        self.x = 0
-        self.y = -1
+        self.id = canvas.create_oval(10,10,25,25, fill=kolor)#reading function canvas.create.oval and their parameters.
+        self.canvas.move(self.id, 245,100)#We are moving the oval to the center of the canvas.
+        begin = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(begin)
+        self.x = begin[0]
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
-    def draw(self):
-        self.canvas.move(self.id, self.x, self.y)#all happends later in the def
-        position = self.canvas.coords(self.id)#creating a variable 'position' and calling out the function 'coords'
-        # and that function gives us back the cords of x y.Where our oval is in the window.
-    if position[1] <= 0:
-        self.y = 1
-    if position[3] >= self.canvas.height:
-        self.y = -1
+        self.canvas_width = self.canvas.winfo_width()
 
-ball = Ball(canvas, 'red')
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)  # all happends later in the def funtion.
+        position = self.canvas.coords(self.id)  # creating a variable 'position' and calling out the function 'coords' and that function gives us back the cords of x y.Where our oval is in the window.
+        if position[1] <= 0:#If position is <= 0 and if the ball hits the top of the screen the ball will stop moving out of our screen
+            self.y = 1
+            canvas.create_text(150, 300, text="Up")
+        if position[3] >= self.canvas_height: #Checking if position is >= canvas.height (y) up and down.
+            self.y = -1
+            canvas.create_text(200, 300, text="Down")
+        if position[0] <= 0:
+            self.x = 3
+            canvas.create_text(300, 100, text="Left")
+        if position[2] >= self.canvas_width:#Checking if positon is >= canvas.with (x) left and right.
+            self.x = -3
+            canvas.create_text(250, 300, text="Right")
+        # print(self.canvas.coords(self.id)) ___________ #Prints out our cords of the ball.
+
+
+
+
+ball = Ball(canvas, 'red')#ball and color
 while 1:#Main loop, contorls most of the program
     ball.draw()
     tk.update_idletasks()
     tk.update()
-    time.sleep(0.01)
-
+    time.sleep(0.01)#Change the speed of the ball.
